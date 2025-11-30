bd9ea38bdb90891c6df34926c844db03a8a4f217
fisay <Devofisayo01@gmail.com>
2025-11-29 19:01:15 -0500

    Added last name

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React+Fisayo</h1>
+      <h1>Vite + React+Fisayo Adepegba</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
